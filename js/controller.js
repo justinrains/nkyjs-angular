d4871eddf25781b9e1a4249cc1ff3bb6e37dbdd1
Justin Rains <justin@justinrains.com>
2015-05-18 15:48:44 -0400

    Adding new people

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,46 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
-    $http.get('js/presenters.json').success(function(data) {
-        $scope.presenters = data;
-//        $scope.presenterOrder = $scope.options[1];
-    });
-}]);
-
+var myApp = angular.module('myApp', [])
+    .controller('MyController', function() {
+      var presenterList = this;
+      presenterList.presenters =  [
+     {
+         "name":        "Mike",
+         "enjoys":      "camping, fishing, target shooting"
+     },
+     {
+            "name":     "Ann",
+            "enjoys":   "Python, online learning"
+        },
+        {
+            "name":     "Richard",
+            "enjoys":   "Cars, money, motors,snow"
+        }/*,
+        {
+            "name":     "Kyle",
+            "enjoys":   "Snow"
+        },
+        {
+            "name":     "Tommy",
+            "enjoys":   "Campaigning, small jackets"
+        },
+        {
+            "name":     "Justin Rains",
+            "enjoys":   "Camping, Python, coding, Raspberry Pi"
+        },
+        {
+            "name":     "Mario",
+            "enjoys":   "Racing, carts, plumbing"
+        }*/
+    ];
+//    $http.get('js/presenters.json').success(function(data) {
+//        $scope.presenters = data;
+////        $scope.presenterOrder = $scope.options[1];
+//    }]);
+      presenterList.addPresenter = function() {
+        presenterList.presenters.push({
+            name: presenterList.presenterText,
+            enjoys: "Stuff"
+        });
+        presenterList.presenterText = '';
+      };
+      });
+//      });
